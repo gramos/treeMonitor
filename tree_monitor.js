f05f819fdd89f735cd670d7167c39433b045f452
Gaston Ramos <ramos.gaston@gmail.com>
2016-04-25 19:19:54 -0300

    Manage changed files;

--- tree_monitor.js
+++ tree_monitor.js
@@ -3,19 +3,38 @@ const
   fs    = require('fs'),
   async = require('async');
 
-var dirContent = [], dirNewContent = [] ;
+var dirContent = [], dirNewContent = [], changedFiles = [], statFiles = {} ;
 
 module.exports = function(dirPath, callback) {
     async.forever( function(next) {
+
+        _.each(dirContent, function(e, index, list) {
+            statFiles[dirPath + '/' + e] = fs.statSync(dirPath + '/' + e)['ctime'].toString() ;
+        });
+
         dirNewContent = fs.readdirSync(dirPath);
 
         var deletedFiles  = _.difference(dirContent, dirNewContent);
         var addedFiles    = _.difference(dirNewContent, dirContent);
+        var changedFilesBuffer = _.intersection(dirNewContent, dirContent);
 
-        if ( deletedFiles.length > 0  || addedFiles.length > 0 ) {
-            callback(dirNewContent, deletedFiles, addedFiles);
+        changedFilesBuffer.forEach(function(e) {
+            if( fs.statSync(dirPath + '/' + e)['ctime'].toString()  !=
+                statFiles[ dirPath + '/' + e ]) {
+                console.log(e);
+                changedFiles.push(e);
+            }
+        });
+
+        //console.log(changedFiles);
+
+        if ( deletedFiles.length > 0  || addedFiles.length > 0 || changedFiles.length > 0) {
+            callback(dirNewContent, deletedFiles, addedFiles, changedFiles);
+            
+
             dirContent = dirNewContent;
         };
+
         next();
 
     },
