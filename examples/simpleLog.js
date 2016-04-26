a98a53ae5cc4cdd32913dd71fefb552ad17f5423
Gaston Ramos <ramos.gaston@gmail.com>
2016-04-26 14:58:51 -0300

    fix example.

--- examples/simpleLog.js
+++ examples/simpleLog.js
@@ -1,7 +1,7 @@
 const
-    treeMonitor = require('./tree_monitor');
+    treeMonitor = require('../tree_monitor');
 
-treeMonitor('./sync', function(dirContent, deleted, added, changed) {
+treeMonitor('../test/sync', function(dirContent, deleted, added, changed) {
     console.log('Deleted files: ' + deleted + '\nAdded Files: '
                 + added + '\nChanged Files: ' + changed);
 })
