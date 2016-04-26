427592c5a25be38059b1276f30499feb6ccfa526
Gaston Ramos <ramos.gaston@gmail.com>
2016-04-26 13:41:20 -0300

    Add test helper method.

--- test/tree_monitor_test.js
+++ test/tree_monitor_test.js
@@ -3,14 +3,21 @@ const
    treeMonitor = require('../tree_monitor'),
    fs          = require('fs');
 
-fs.writeFile("./sync/file1.txt", "Hey there!", function(err) {});
-fs.writeFile("./sync/file2.txt", "Hey there!", function(err) {});
 
-var dirContent, deleted, added, changed;
+function test(name, cb){
+    cb();
+    console.log("\033[0;32m.");
+}
 
-treeMonitor('./sync', function(_dirContent, _deleted, _added, _changed) {
-    dirContent = _dirContent;
-}, "true")
+test('First time all existing files are added', function() {
+    fs.writeFile("./sync/file1.txt", "Hey there!", function(err) {});
+    fs.writeFile("./sync/file2.txt", "Hey there!", function(err) {});
 
-assert.deepEqual( dirContent, ['file1.txt', 'file2.txt'] );
+    var dirContent, deleted, added, changed;
 
+    treeMonitor('./sync', function(_dirContent, _deleted, _added, _changed) {
+        dirContent = _dirContent;
+    }, "true")
+
+    assert.deepEqual( dirContent, ['.gitignore', 'file1.txt', 'file2.txt'] );
+});
